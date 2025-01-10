5927dea5869e871b249431cec0944c6a53793340
Enzo Massyle <enzo.massyle@berkeley.edu>
2025-01-10 12:01:55 -0800

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     char parentFolder[16] = "../samples/";
     char *fn = strcat(parentFolder, argv[1]);
     // Perform Autotune in C Major Scale
-    // Autotune *obj = new Autotune(1,  "A", 'm');
+    // Autotune *obj = new Autotune(1,  "C", 'M');
     // obj->fillNoteTable();
     // obj->process(fn);
 
