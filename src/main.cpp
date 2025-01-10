0dbe1eb5c9b1cb62b61d255d109ebd94e9dc4f9c
Enzo Massyle <enzo.massyle@berkeley.edu>
2025-01-10 11:59:39 -0800

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,16 @@ int main(int argc, char *argv[])
     char parentFolder[16] = "../samples/";
     char *fn = strcat(parentFolder, argv[1]);
     // Perform Autotune in C Major Scale
-    Autotune *obj = new Autotune(1,  "A", 'm');
-    obj->fillNoteTable();
-    obj->process(fn);
-    // TimeShift::changeSpseed(fn, 1.2);
-    // PitchShift::changePitch(fn, 0.93);
-    // Reverb::apply(fn, "AIRY");
+    // Autotune *obj = new Autotune(1,  "A", 'm');
+    // obj->fillNoteTable();
+    // obj->process(fn);
+
+    // change second parameter to affect playback speed
+    // TimeShift::changeSpseed(fn, 1);
+
+    // change second parameter to affect pitch
+    // PitchShift::changePitch(fn, 1);
+
+
+    // Reverb::apply(fn);
 }
