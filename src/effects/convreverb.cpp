f01ecde3d1261e48aaa140e1ec03a37f3172ea21
Enzo Massyle <enzo.massyle@berkeley.edu>
2025-03-05 18:37:31 -0800

    move impulse response to assets and package them

--- src/effects/convreverb.cpp
+++ src/effects/convreverb.cpp
@@ -1,8 +1,8 @@
 #include "convreverb.h"
 unordered_map<string, string> Reverb::types = {
-    {"CHURCH", "church.wav"},
-    {"CAVE", "cave.wav"},
-    {"AIRY", "airy.wav"}};
+    {"CHURCH", "assets/ir/church.wav"},
+    {"CAVE", "assets/ir/cave.wav"},
+    {"AIRY", "assets/ir/airy.wav"}};
 
 vector<vector<double>> Reverb::convReverb(vector<vector<double>>& samples, string room)
 {
